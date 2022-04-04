312ee69a63afdda4477d30b63b8dff1ffd356859
israel-marques <devisraelmarques@gmail.com>
2022-04-04 09:51:13 -0300

    ajustando mensagem de erro

--- config.js
+++ config.js
@@ -1,56 +1,56 @@
 var btn = document.querySelector("#pesquisa");
-var produto=document.querySelector("#conteudo")
-var arrayH5=document.querySelectorAll("h5")
-var arrayP=document.querySelectorAll("p")
+var produto = document.querySelector("#conteudo");
+var arrayH5 = document.querySelectorAll("h5");
+var arrayP = document.querySelectorAll("p");
 var produtos;
-var footer=document.querySelector("footer")
-var msgErro=document.querySelector("#erro")
-
-
-
+var footer = document.querySelector("footer");
+var msgErro = document.querySelector("#erro");
 
 btn.addEventListener("click", (event) => {
   event.preventDefault();
   let termo = document.querySelector("#termo").value.split(" ");
-  termo=termo[0]
-  const options={
-      method:"GET",
-      mode:"cors",
-      cache:"default"
-  }  
-   fetch(`https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${termo}&source=nanook`, options)
-   .then((response)=>{response.json().then((data) =>{
-if(response.status==200){
-  produtos=data.products;
-  removeClass()
- pegaElementos()
-}else{
-  msgErro.classList.remove('d-none'); 
-  addClass()
-}
- 
-  })
-   })
-   .catch(((erro)=>{console.log("Deu erro:", + erro,message);
-  })
-   )
+  termo = termo[0];
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+  fetch(
+    `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${termo}&source=nanook`,
+    options
+  )
+    .then((response) => {
+      response.json().then((data) => {
+        if (response.status == 200) {
+          produtos = data.products;
+          removeClass();
+          pegaElementos();
+          msgErro.classList.add("d-none");
+        } else {
+          msgErro.classList.remove("d-none");
+        }
+      });
+    })
+    .catch((erro) => {
+      console.log("Deu erro:", +erro, message);
+    });
 });
 
-
-function pegaElementos(){
-  for(let i=0; i<=5; i++){
-    let nome= produtos[i].name 
-    let id= produtos[i].id
-    arrayH5[i].textContent=nome 
-    arrayP[i].textContent=id 
-  }}
-
-  function removeClass(){
-    produto.classList.remove('d-none');
-    footer.classList.remove('posicao');
+function pegaElementos() {
+  for (let i = 0; i <= 5; i++) {
+    let nome = produtos[i].name;
+    let id = produtos[i].id;
+    arrayH5[i].textContent = nome;
+    arrayP[i].textContent = id;
   }
-  
-  function addClass(){
-    produto.classList.add('d-none');
-    footer.classList.add('posicao');
-  }
+}
+
+function removeClass() {
+  produto.classList.remove("d-none");
+  footer.classList.remove("posicao");
+}
+
+function addClass() {
+  produto.classList.add("d-none");
+  footer.classList.add("posicao");
+}
